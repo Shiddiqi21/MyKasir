119cb05f877faba4e092abdf2b5888786800e6dd
iqbal-cloud-ux <ahmadiqbalramadhan6@gmail.com>
2025-10-28 10:09:39 +0700

    save progress before switching branch

--- app/src/main/java/com/example/mykasir/feature_manajemen_produk/screen/ManajemenProdukScreen.kt
+++ app/src/main/java/com/example/mykasir/feature_manajemen_produk/screen/ManajemenProdukScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -173,3 +174,4 @@ fun ManajemenProdukScreen(navController: NavController, viewModel: ProductViewMo
         }
     }
 }
+
